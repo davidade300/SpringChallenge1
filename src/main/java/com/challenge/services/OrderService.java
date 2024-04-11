ce041182b645da160a028d1861e85b23092ebb59
davidade300 <iaderinho@outlook.com>
2024-04-11 10:17:22 -0300

    Order and shipping services instantiated

--- src/main/java/com/challenge/services/OrderService.java
+++ src/main/java/com/challenge/services/OrderService.java
@@ -1,7 +1,15 @@
 package com.challenge.services;
 
+import com.challenge.entities.Order;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    private ShippingService shippingService;
+
+    public double total(Order order) {
+        return  order.getBasic() + shippingService.shipment(order)
+                - order.getDiscount();
+    }
 }
--- src/main/java/com/challenge/services/ShippingService.java
+++ src/main/java/com/challenge/services/ShippingService.java
@@ -7,7 +7,15 @@ import org.springframework.stereotype.Service;
 public class ShippingService {
 
 
-    public double total(Order order) {
-
+    public double shipment(Order order) {
+        if (order.getBasic() < 100.00) {
+            return 20.00;
+        }
+        else if (order.getBasic() >= 100.00 & order.getBasic() < 200.00) {
+            return 12.00;
+        }
+        else {
+            return 0.0;
+        }
     }
 }
